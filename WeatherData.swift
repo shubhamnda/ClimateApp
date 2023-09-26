1e9cb77b5191af0747071a6f9cf6ef97ce41132b
shubham nanda <shubhamn.ec.22@gmail.com>
2023-09-26 17:42:26 +0530

    weather

---
+++ WeatherData.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Shubham Nanda on 13/07/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+struct WeatherData: Codable {
+    let name: String
+    let main:Main
+    let weather: [Weather]
+}
+struct Main : Codable {
+    let temp: Double
+}
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
